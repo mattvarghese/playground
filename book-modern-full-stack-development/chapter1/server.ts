72212aa7da37b3945964436c3455c847a7bac82b
Matt Varghese <16229800+mattvarghese@users.noreply.github.com>
2023-03-13 00:27:56 -0500

    minor formatting

--- book-modern-full-stack-development/chapter1/server.ts
+++ book-modern-full-stack-development/chapter1/server.ts
@@ -2,9 +2,12 @@ import http from 'http';
 import request from 'request';
 
 function RequestHandler(_inRequest: http.IncomingMessage, inResponse: http.ServerResponse) {
-    request("http://worldtimeapi.org/api/timezone/America/Chicago", (_inErr,_inResp,inBody) => {
-        inResponse.end(`Hello from my first Node Web Server: ${inBody}`);
-    });
+    request(
+        "http://worldtimeapi.org/api/timezone/America/Chicago",
+        (_inErr: any, _inResp: request.Response, inBody: any) => {
+            inResponse.end(`Hello from my first Node Web Server: ${inBody}`);
+        }
+    );
 }
 
 const server = http.createServer(RequestHandler);
