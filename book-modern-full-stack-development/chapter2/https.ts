30793647b419286b644fae3b69805c64858e5905
Matt Varghese <16229800+mattvarghese@users.noreply.github.com>
2023-03-26 00:37:20 -0500

    querystring and url modules

--- book-modern-full-stack-development/chapter2/https.ts
+++ book-modern-full-stack-development/chapter2/https.ts
@@ -4,13 +4,15 @@
 
 import https from 'https';
 import fs from 'fs/promises';
+import url from 'url';
+import querystring from 'querystring';
 
-
-function readWolrdTime(): Promise<string> {
+function readWolrdTime(zone: string): Promise<string> {
+    if (zone == "") zone = "Chicago";
     const requestOptions: https.RequestOptions = {
         hostname: "worldtimeapi.org",
         port: 443,
-        path: "/api/timezone/America/Chicago",
+        path: "/api/timezone/America/" + zone,
         method: "GET"
     };
     return new Promise<string>((resolve) => {
@@ -39,10 +41,15 @@ async function startServer() {
         fs.readFile('certificate.pem')
     ]);
     https.createServer({ key, cert }, async (req, res) => {
+        const parsedURL = url.parse(req.url as string, true);
+        console.log("\nURL: ", req.url);
+        console.log("\nParsed URL: " + JSON.stringify(parsedURL,null,2));
+        console.log("\nHeaders: \n" + JSON.stringify(req.headers,null,2))
+        const timeZone: string = parsedURL.query.timezone as string ?? "";
         res.statusCode = 200;
-        const jsonString = await readWolrdTime();
+        const jsonString = await readWolrdTime(timeZone);
         const jsonObj = JSON.parse(jsonString);
-        res.end(JSON.stringify(jsonObj, null, 2));
+        res.end("Time API response as JSON: \n" + JSON.stringify(jsonObj, null, 2) + "\nAs Query String: \n" + querystring.stringify(jsonObj));
     })
         // Like with Visual Studio IISExpress, it doesn't seem to work if we use
         // prots other than 443XX 
@@ -54,3 +61,7 @@ async function startServer() {
 startServer();
 
 // Navigate to https://localhost:44301/
+// or https://localhost:44301/?timezone=New_York
+// or https://localhost:44301/?timezone=Menominee
+// or https://localhost:44301/some/path?timezone=Menominee
+// See: https://worldtimeapi.org/api/timezone/America/ for timezone options
