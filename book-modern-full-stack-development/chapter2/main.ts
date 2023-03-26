dbd5ef977d1f3260c2f4649d98d74b4894bb3936
Matt Varghese <16229800+mattvarghese@users.noreply.github.com>
2023-03-25 22:26:08 -0500

    fs/promises and getting directory sizes

--- book-modern-full-stack-development/chapter2/main.ts
+++ book-modern-full-stack-development/chapter2/main.ts
@@ -1,23 +1,39 @@
-import fs from 'fs';
+//import fs from 'fs';  // prefer promises version in TypeScript
+import fs from "fs/promises";
+import { Stats } from "fs";
 import path from "path";
-import { readdir, stat } from "fs/promises";
 
 async function main(): Promise<void> {
-    let data = fs.readdirSync(".");
+    const data = await fs.readdir(".");
 
     data.forEach(element => {
         console.log(element);
     });
 
-    let stat = fs.statSync(".");
-    console.log("BirthTime: " + stat.birthtime);
+    const stat = await fs.stat(".");
+    console.log("\nBirthTime: " + stat.birthtime);
     console.log("IsFile: " + stat.isFile());
     console.log("Size: " + await dirSize(".") + " bytes");
 }
 
 const dirSize = async (directory: string) => {
-    const files = await readdir(directory);
-    const stats = files.map(file => stat(path.join(directory, file)));
+    const files = await fs.readdir(directory);
+    let size: number = 0;
+    for (const file of files) {
+        const filePath = path.join(directory, file);
+        const stat = await fs.stat(filePath);
+        if (stat.isFile()) {
+            size += stat.size;
+        } else {
+            size += await dirSize(filePath);
+        }
+    }
+    return size;
+}
+
+const dirSizeBad = async (directory: string) => {
+    const files = await fs.readdir(directory);
+    const stats = files.map(file => fs.stat(path.join(directory, file)));
     return (await Promise.all(stats)).reduce((accumulator, { size }) => accumulator + size, 0);
 }
 
