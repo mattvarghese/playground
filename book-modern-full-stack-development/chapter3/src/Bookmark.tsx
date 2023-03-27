3517ca93f94eb5c8f7f9700c2b09481a7183e829
Matt Varghese <16229800+mattvarghese@users.noreply.github.com>
2023-03-27 00:57:10 -0500

    chapter 3 working, but no styling

---
+++ book-modern-full-stack-development/chapter3/src/Bookmark.tsx
@@ -0,0 +1,44 @@
+// Copyright (C) 2022 Varghese Mathew (Matt)
+
+import React from "react";
+
+interface IProps {
+    title: string;
+    href: string;
+    description: string;
+}
+interface IState {
+    title: string;
+}
+
+class Bookmark extends React.Component<IProps, IState> {
+
+    // The explicit override of state is not required 
+    // As we passed the state type in the template
+    // public override state:IState;
+
+    constructor(props: IProps) {
+        super(props);
+        this.state = { title: props.title };
+
+        // Ref: https://stackoverflow.com/questions/70634283/react-typescript-uncaught-typeerror-this-is-undefined
+        this.buttonOnClickEvent = this.buttonOnClickEvent.bind(this);
+    }
+
+    private buttonOnClickEvent(): void {
+        const newState: IState = { title: this.state.title + "-CHANGED" };
+        this.setState(newState);
+    }
+
+    public override render(): React.ReactNode {
+        return (
+            <li>
+                <h2>{this.state.title}</h2>
+                <a href={this.props.href}>{this.props.description}</a>
+                <button onClick={this.buttonOnClickEvent}>Change Title</button>
+            </li>
+        );
+    }
+}
+
+export default Bookmark;
--- book-modern-full-stack-development/chapter3/src/index.tsx
+++ book-modern-full-stack-development/chapter3/src/index.tsx
@@ -2,10 +2,16 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import Bookmark from "./Bookmark";
 import "./index.css";
 
 // Reference: https://github.com/reactwg/react-18/discussions/5
 const container = document.querySelector("#root");
 const root = ReactDOM.createRoot(container as HTMLElement);
-root.render(<App heading="Hello React" body="Hope you have a ton of fun!" />,);
+root.render(
+    <>
+        <Bookmark title="Google" href="https://www.google.com" description="Google Search Engine" />
+        <Bookmark title="Wikipedia" href="https://en.wikipedia.org" description="Wikipedia Internet Encyclopedia" />
+        <Bookmark title="GitHub" href="https://github.com/" description="GitHub Source Code Repositories" />
+    </>
+);
